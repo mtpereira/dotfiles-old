279064f8475bfce0949087975b3213e78a69389c
Manuel Tiago Pereira <mt.pereira@gmail.com>
2012-04-28 02:23:38 +0100

    configure.sh: Script for installing the dotfiles using simlinks.

---
+++ configure.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set error
+set unused
+
+die() {
+echo "$1" >&2  && exit 1
+}
+
+[ $(basename $PWD) = ".dotfiles" ] || die "$0: not on .dotfiles directory, exiting." 
+
+IGNORE=".git .svn"
+FIGNORE=$(echo $IGNORE | awk 'BEGIN { RS=" "; FS="\n"; } { print "! -name ", $1 }')
+FILES=$(find . -xdev -maxdepth 1 ${FIGNORE} ! -executable -type f -printf "%f ")
+
+for file in ${FILES}; do mv "$HOME/$file" "$PWD/$file.safe" && ln -s "$PWD/$file" "$HOME/$file" && rm "$PWD/$file.safe"; done
+
