38ffc790793e9a8289f09437aabd8c05a0a1d029
Manuel Tiago Pereira <mt.pereira@gmail.com>
2013-03-17 20:08:10 +0000

    Deprecated configure.sh.

--- configure.sh
+++ configure.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# DEPRECATED!
+
 set error
 set unused
 
 die() {
-echo "$1" >&2  && exit 1
+	echo "$0: $1" >&2 
+	exit 1
 }
 
 [ $(basename $PWD) = ".dotfiles" ] || die "$0: not on .dotfiles directory, exiting." 
@@ -13,5 +16,8 @@ IGNORE=".git .svn"
 FIGNORE=$(echo $IGNORE | awk 'BEGIN { RS=" "; FS="\n"; } { print "! -name ", $1 }')
 FILES=$(find . -xdev -maxdepth 1 ${FIGNORE} ! -executable -type f -printf "%f ")
 
-for file in ${FILES}; do mv "$HOME/$file" "$PWD/$file.safe" ; ln -s "$PWD/$file" "$HOME/$file" && rm "$PWD/$file.safe"; done
+for file in ${FILES}; do 
+	mv "$HOME/$file" "$PWD/$file.safe"
+	ln -s "$PWD/$file" "$HOME/$file" && rm "$PWD/$file.safe"
+done
 
