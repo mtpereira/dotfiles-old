dbb942d6748b198eebb5d0595c4e4bf527548aed
Manuel Tiago Pereira <mt.pereira@gmail.com>
2016-12-07 10:12:41 +0000

    Remove JS support from YouCompleteMe.

--- .vim/update.sh
+++ .vim/update.sh
@@ -76,7 +76,7 @@ case "$1" in
       echo "Cloning $url into $dest"
       git clone --recurse-submodules --depth 1 $url $dest
 	  if [ "$url" == "https://github.com/Valloric/YouCompleteMe.git" ]; then
-		$dest/install.py --clang-completer --gocode-completer --tern-completer
+		$dest/install.py --clang-completer --gocode-completer
 	  fi
       rm -rf $dest/.git
     done
